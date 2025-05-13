41f74fd615a60e8030a5a78f1e132b8e88ff9fae
zhiltsovdima <50846656+zhiltsovdima@users.noreply.github.com>
2025-05-13 10:06:23 +0300

    feat: project init

---
+++ Sweather Weather/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Sweather Weather
+//
+//  Created by Dima Zhiltsov on 13.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
