19d2cc8ff8fb57cd6d79c219387b75ca6eee6566
zhiltsovdima <50846656+zhiltsovdima@users.noreply.github.com>
2025-05-14 09:46:37 +0300

    feat: location service async/await

--- Sweather Weather/Services/LocationService.swift
+++ Sweather Weather/Services/LocationService.swift
@@ -5,48 +5,97 @@
 //  Created by Dima Zhiltsov on 13.05.2025.
 //
 
-import Foundation
 import CoreLocation
 
 protocol LocationService {
-    func requestLocation(completion: @escaping (CLLocation?) -> Void)
+    func getCurrentLocation() async throws -> UserLocation
+}
+
+enum LocationError: Error {
+    case accessDenied
+    case locationUnavailable
+    case unknown
 }
 
 final class LocationServiceImpl: NSObject, LocationService {
     
-    private let locationManager = CLLocationManager()
-    private var locationRequestCompletion: ((CLLocation?) -> Void)?
+    private let locationManager: CLLocationManager
+    
+    private var authorizationContinuation: CheckedContinuation<CLAuthorizationStatus, Never>?
+    private var locationContinuation: CheckedContinuation<CLLocation, Error>?
     
     override init() {
+        locationManager = CLLocationManager()
         super.init()
         locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
-        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
     }
     
-    func requestLocation(completion: @escaping (CLLocation?) -> Void) {
-        locationRequestCompletion = completion
-        locationManager.requestLocation()
+    func getCurrentLocation() async throws -> UserLocation {
+        let status = locationManager.authorizationStatus
+        switch status {
+        case .notDetermined:
+            return try await waitForAuthorizationAndGetLocation()
+        case .restricted, .denied:
+            throw LocationError.accessDenied
+        case .authorizedWhenInUse:
+            return try await requestLocation()
+        default:
+            throw LocationError.unknown
+        }
     }
     
+    private func waitForAuthorizationAndGetLocation() async throws -> UserLocation {
+        let status = await withCheckedContinuation { continuation in
+            self.authorizationContinuation = continuation
+            locationManager.requestWhenInUseAuthorization()
+        }
+        
+        switch status {
+        case .authorizedWhenInUse:
+            return try await requestLocation()
+        case .denied, .restricted:
+            throw LocationError.accessDenied
+        default:
+            throw LocationError.unknown
+        }
+    }
+    
+    private func requestLocation() async throws -> UserLocation {
+        let location = try await withCheckedThrowingContinuation { continuation in
+            self.locationContinuation = continuation
+            locationManager.requestLocation()
+        }
+        
+        return UserLocation(
+            latitude: location.coordinate.latitude,
+            longitude: location.coordinate.longitude
+        )
+    }
+
 }
 
 // MARK: - CLLocationManagerDelegate
 
 extension LocationServiceImpl: CLLocationManagerDelegate {
     
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        authorizationContinuation?.resume(returning: manager.authorizationStatus)
+        authorizationContinuation = nil
+    }
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else {
-            locationRequestCompletion?(nil)
-            return
+        if let location = locations.last {
+            locationContinuation?.resume(returning: location)
+        } else {
+            locationContinuation?.resume(throwing: LocationError.locationUnavailable)
         }
-        locationRequestCompletion?(location)
-        locationRequestCompletion = nil
+        locationContinuation = nil
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print(error.localizedDescription)
-        locationRequestCompletion?(nil)
-        locationRequestCompletion = nil
+        locationContinuation?.resume(throwing: LocationError.locationUnavailable)
+        locationContinuation = nil
     }
+    
 }
